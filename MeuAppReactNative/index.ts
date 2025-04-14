a4008c0c788bb1468a14b9d13fed0f769dbd55e4
RafaellSouzaPinto <rafaelsouza.rs14@gmail.com>
2025-04-13 21:27:11 -0300

    Projeto Finalizado

--- MeuAppReactNative/index.ts
+++ MeuAppReactNative/index.ts
@@ -1,6 +1,9 @@
 import { registerRootComponent } from "expo";
+export { Appointment } from "./src/types/appointments";
+export { Doctor } from "./src/types/doctors";
+export { RootStackParamList } from "./src/types/navigation";
 
-import App from "./src/App";
+import App from "./App";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
